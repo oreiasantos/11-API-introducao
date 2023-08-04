fd864d52c16c97e635e54c7ee8bac325931905dc
oreiasantos <132698405+oreiasantos@users.noreply.github.com>
2023-08-04 17:35:03 +0000

    atualiza

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import { selectUsuarios } from "./bd.js";
 import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
@@ -11,4 +14,14 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
